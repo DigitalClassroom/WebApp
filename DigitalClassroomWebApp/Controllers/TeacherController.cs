1f41317ec6654626380856302316ab7b7e91501e
Klaus <corb-bartloff@web.de>
2020-03-22 10:12:55 +0100

    First draft

---
+++ DigitalClassroomWebApp/Controllers/TeacherController.cs
@@ -0,0 +1,18 @@
+﻿using DigitalClassroomWebApp.DataPresenter;
+using DigitalClassroomWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalClassroomWebApp.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index(int id = 0)
+        {
+            return View();
+        }
+    }
+}
