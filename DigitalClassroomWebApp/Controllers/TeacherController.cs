26ba2aec943d6230c5fcb5f9d6028f2421a6e582
Klaus <corb-bartloff@web.de>
2020-03-22 13:51:00 +0100

    Schedule implemented

--- DigitalClassroomWebApp/Controllers/TeacherController.cs
+++ DigitalClassroomWebApp/Controllers/TeacherController.cs
@@ -2,9 +2,6 @@
 using DigitalClassroomWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DigitalClassroomWebApp.Controllers
 {
@@ -12,7 +9,40 @@ namespace DigitalClassroomWebApp.Controllers
     {
         public IActionResult Index(int id = 0)
         {
-            return View();
+            TeacherDashboard model = new TeacherDashboard();
+            model.Schedules.Add(new ScheduleItem()
+            {
+                Title = "Englisch\n6a",
+                DayOfWeek = DayOfWeek.Monday,
+                TimeSlot = 1,
+                CssTag = "Aqua",
+            });
+
+            model.Schedules.Add(new ScheduleItem()
+            {
+                Title = "Englisch\n8a",
+                DayOfWeek = DayOfWeek.Monday,
+                TimeSlot = 2,
+                CssTag = "Aqua",
+            });
+
+            model.Schedules.Add(new ScheduleItem()
+            {
+                Title = "Biologie\n6a",
+                DayOfWeek = DayOfWeek.Monday,
+                TimeSlot = 3,
+                CssTag = "Lime",
+            });
+
+            model.Schedules.Add(new ScheduleItem()
+            {
+                Title = "Englisch\n5b",
+                DayOfWeek = DayOfWeek.Monday,
+                TimeSlot = 6,
+                CssTag = "Aqua",
+            });
+
+            return View(model);
         }
     }
 }
---
+++ DigitalClassroomWebApp/Models/TeacherDashboard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalClassroomWebApp.Models
+{
+    public class TeacherDashboard
+    {
+        public List<ScheduleItem> Schedules { get; set; } = new List<ScheduleItem>();
+    }
+}
